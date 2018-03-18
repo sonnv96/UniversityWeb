f2694d311985c215568c4f7e172f881d62456d0c
sonnv <sonnv@DESKTOP-HF68BBN>
2018-03-18 22:37:32 +0700

    them xoa sưa bai viết

---
+++ UniversityWeb/UniversityWeb/Controllers/BaiVietsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UniversityWeb.Models.Data;
+
+namespace UniversityWeb.Controllers
+{
+    public class BaiVietsController : Controller
+    {
+        private UniversityEntities db = new UniversityEntities();
+
+        // GET: BaiViets
+        public ActionResult Index()
+        {
+            return View(db.BaiViets.ToList());
+        }
+
+        // GET: BaiViets/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BaiViet baiViet = db.BaiViets.Find(id);
+            if (baiViet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(baiViet);
+        }
+
+        // GET: BaiViets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BaiViets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "maBaiViet,tieuDe,noiDung,ngayDang")] BaiViet baiViet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BaiViets.Add(baiViet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(baiViet);
+        }
+
+        // GET: BaiViets/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BaiViet baiViet = db.BaiViets.Find(id);
+            if (baiViet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(baiViet);
+        }
+
+        // POST: BaiViets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "maBaiViet,tieuDe,noiDung,ngayDang")] BaiViet baiViet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(baiViet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(baiViet);
+        }
+
+        // GET: BaiViets/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BaiViet baiViet = db.BaiViets.Find(id);
+            if (baiViet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(baiViet);
+        }
+
+        // POST: BaiViets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            BaiViet baiViet = db.BaiViets.Find(id);
+            db.BaiViets.Remove(baiViet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
